76934e275f931c167acf9b69d25761fa20719157
MaChi <loyuk@sina.com>
2015-09-18 17:15:58 +0800

    dev
    增加Dialog和Tips的接口

--- src/js/components/helloworld.js
+++ src/js/components/helloworld.js
@@ -72,7 +72,18 @@ define(function (require, exports, module){
 
             $titlePanel.html((title || '无标题'));
             $contentPanel.html(MarkdownParser(text));
+        },
+
+        //获得弹出框。该弹出框是单例模式，只允许一个弹出框显示。
+        _getDialog: function (){
+
+        },
+
+        //获得提示框。该提示框是单例模式，只允许一个提示框显示。
+        _getTips: function (){
+            
         }
+
     });
 
     //外部方法
